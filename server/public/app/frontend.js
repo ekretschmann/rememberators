77f7582a27fa5e114f85157817818828c52d7e61
ernst <ernst.kretschmann@gmail.com>
2017-02-20 15:12:31 +0000

    initial checkin

---
+++ server/public/app/frontend.js
@@ -0,0 +1,1 @@
+webpackJsonp([1],{262:function(n,o,e){"use strict";function t(n){return n&&n.__esModule?n:{default:n}}var u=e(350);t(u);console.log("Frontend loaded!")},350:function(n,o){},576:function(n,o,e){n.exports=e(262)}},[576]);
